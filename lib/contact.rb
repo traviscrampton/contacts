9cc77204f9ed1da684b1b74185353ebf38756c6d
Travis Crampton & Brian Lau <student@epicodus.com>
2015-08-13 10:56:28 -0700

    add specs for clear save and all

--- lib/contact.rb
+++ lib/contact.rb
@@ -6,16 +6,16 @@ class Contact
   define_method(:initialize) do |attributes|
     @first_name = attributes.fetch(:first_name)
     @last_name = attributes.fetch(:last_name)
-    # @company = attributes.fetch(:company)
-    # @job_title = attributes.fetch(:job_title)
-    # @email = attributes.fetch(:email)
-    # @phone_number = attributes.fetch(:phone_number)
-    # @phone_type = attributes.fetch(:phone_type)
-    # @street_address = attributes.fetch(:street_address)
-    # @city = attributes.fetch(:city)
-    # @state = attributes.fetch(:state)
-    # @zip_code = attributes.fetch(:zip_code)
-    # @building_type = attributes.fetch(:building_type)
+    @company = attributes.fetch(:company)
+    @job_title = attributes.fetch(:job_title)
+    @email = attributes.fetch(:email)
+    @phone_number = attributes.fetch(:phone_number)
+    @phone_type = attributes.fetch(:phone_type)
+    @street_address = attributes.fetch(:street_address)
+    @city = attributes.fetch(:city)
+    @state = attributes.fetch(:state)
+    @zip_code = attributes.fetch(:zip_code)
+    @building_type = attributes.fetch(:building_type)
     @id = @@contacts.length().+1
   end
 
--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -12,4 +12,25 @@ describe(Contact) do
       expect(test_contact.first_name()).to(eq("Brian"))
     end
   end
+
+  describe('.all') do
+    it('is empty at first') do
+      expect(Contact.all()).to(eq([]))
+    end
+  end
+
+  describe('.clear') do
+    it("empties out all of the saved contacts") do
+      Contact.new({:first_name =>"Brian", :last_name => "Lau", :company => "Epicodus", :job_title => "Student", :email => "blau08@gmail.com", :phone_number => "5037208554", :phone_type => "Cell", :street_address => "10336 SE Quail Ridge Dr", :city => "Happy Valley", :state => "OR", :zip_code => "97086", :building_type => "Home"}).save()
+    Contact.clear()
+    expect(Contact.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it('adds a contact to the array of saved contacts') do
+      test_contact = Contact.new({:first_name =>"Brian", :last_name => "Lau", :company => "Epicodus", :job_title => "Student", :email => "blau08@gmail.com", :phone_number => "5037208554", :phone_type => "Cell", :street_address => "10336 SE Quail Ridge Dr", :city => "Happy Valley", :state => "OR", :zip_code => "97086", :building_type => "Home"}).save()
+      expect(Contact.all()).to(eq(test_contact))
+    end
+  end
 end
