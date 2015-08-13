a9e3461ce3154b42f0da94715926179c677e5504
Travis Crampton & Brian Lau <student@epicodus.com>
2015-08-13 11:16:23 -0700

    add content to index

---
+++ app.rb
@@ -0,0 +1,13 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/contact')
+require('./lib/phonebook')
+
+get('/') do
+  erb(:index)
+end
+
+get('/phonebooks/new') do
+  erb(:phonebooks_form)
+end
